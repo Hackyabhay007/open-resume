35f36cce55c03187aad90d57b6f1d2e5b0f87851
Abhay gupta <hackyabhay@gmail.com>
2025-05-10 01:26:12 +0530

    fix: restructure Home component to use Redux provider and include ResumeForm and Resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,26 @@ import { Steps } from "home/Steps";
 import { Features } from "home/Features";
 import { Testimonials } from "home/Testimonials";
 import { QuestionsAndAnswers } from "home/QuestionsAndAnswers";
+import { Provider } from "react-redux";
+import { ResumeForm } from "components/ResumeForm";
+import { Resume } from "components/Resume";
+import { store } from "lib/redux/store";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-screen-2xl bg-dot px-8 pb-32 text-gray-900 lg:px-12">
-      <Hero />
-      <Steps />
-      <Features />
-      <Testimonials />
-      <QuestionsAndAnswers />
+    <Provider store={store}>
+      <main className="relative h-full w-full overflow-hidden bg-gray-50">
+        <div className="grid grid-cols-3 md:grid-cols-6">
+          <div className="col-span-3">
+            <ResumeForm />
+          </div>
+          <div className="col-span-3">
+            <Resume />
+          </div>
+        </div>
+      </main>
+    </Provider>
     </main>
   );
 }
